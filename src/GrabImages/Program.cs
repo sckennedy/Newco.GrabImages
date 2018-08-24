2998fcf3e3afb0fa9f0918b18cee2368e8198802
Simon Kennedy <simon@skennedy.co.uk>
2018-08-24 10:21:47 +0100

    Added logic to check if file exists before downloading to only get the delta

--- src/GrabImages/Program.cs
+++ src/GrabImages/Program.cs
@@ -7,48 +7,53 @@ namespace GrabImages
 {
     class Program
     {
-        private const string baseUrl = "http://wwwin.cisco.com/dir/photo/zoom/";
-        private static string imagesFolder = @"C:\Code\Images";
-        private static string usernamesFile = @"C:\code\usernames.txt";
-        private static List<string> notFound = new List<string>();
+        private const string BaseUrl = "http://wwwin.cisco.com/dir/photo/zoom/";
+        private static string _imagesFolder = @"C:\Code\Images";
+        private static string _usernamesFile = @"C:\code\usernames.txt";
+        private static readonly List<string> NotFound = new List<string>();
+        public static int AlreadyExistsCount = 0;
         
         static void Main(string [] args)
         {
             if (!string.IsNullOrEmpty(args[0]))
-                imagesFolder = args[0];
+                _imagesFolder = args[0];
 
             if (!string.IsNullOrEmpty(args[1]))
-                usernamesFile = args[1];
+                _usernamesFile = args[1];
 
             GetImages();
         }
 
         static void GetImages()
         {
-            if (!Directory.Exists(imagesFolder))
-                Directory.CreateDirectory(imagesFolder);
+            if (!Directory.Exists(_imagesFolder))
+                Directory.CreateDirectory(_imagesFolder);
 
-            if(!File.Exists(usernamesFile))
+            if(!File.Exists(_usernamesFile))
             {
-                Console.WriteLine($"{usernamesFile} does not exist.  Terminating.");
+                Console.WriteLine($"{_usernamesFile} does not exist.  Terminating.");
                 Console.ReadLine();
                 return;
             }
 
-            string[] usernames = File.ReadAllLines(usernamesFile);
+            string[] usernames = File.ReadAllLines(_usernamesFile);
 
             foreach (string username in usernames)
             {
                 DownloadImage(username);
             }
 
-            Console.WriteLine($"Images downloaded to {imagesFolder}");
-            if (notFound.Count > 0)
+            Console.WriteLine($"Images downloaded to {_imagesFolder}");
+            if (NotFound.Count > 0)
             {
-                var fileName = WriteOutputToFile(notFound, "Images_Not_Found");
+                var fileName = WriteOutputToFile(NotFound, "Images_Not_Found");
                 Console.WriteLine($"Images not found  file created {fileName}");
             }
-            
+
+            Console.WriteLine($"{usernames.Length} Usernames.");
+            Console.WriteLine($"{AlreadyExistsCount} images already downloaded.");
+            Console.WriteLine($"{usernames.Length - AlreadyExistsCount} new downloads.");
+
             Console.WriteLine("Press any key to exit.");
             Console.ReadKey();
         }
@@ -56,26 +61,39 @@ namespace GrabImages
         private static string WriteOutputToFile(List<string> lines, string name)
         {
             //create filename based on timestamp of the day
-            var fileName = $@"{imagesFolder}\{name}_{DateTime.Now.ToShortDateString().Replace("/", "_")}-{DateTime.Now.ToShortTimeString().Replace(":", "-")}.txt";
+            var fileName = $@"{_imagesFolder}\{name}_{DateTime.Now.ToShortDateString().Replace("/", "_")}-{DateTime.Now.ToShortTimeString().Replace(":", "-")}.txt";
             File.WriteAllLines(fileName, lines.ToArray());
             return fileName;
         }
 
         private static void DownloadImage(string username)
         {
-            string fileName = $"{username}.jpg", myStringWebResource = null;
-            try
+            var fileName = $"{username}.jpg";
+
+            if (!ImageExists(username))
             {
-                WebClient myWebClient = new WebClient();
-                myStringWebResource = baseUrl + fileName;
-                myWebClient.DownloadFile(myStringWebResource, $"{imagesFolder}/{fileName}");
-                Console.WriteLine($"{username} done");
+                var myWebClient = new WebClient();
+                try
+                {
+                    var myStringWebResource = BaseUrl + fileName;
+                    myWebClient.DownloadFile(myStringWebResource, $"{_imagesFolder}/{fileName}");
+                    Console.WriteLine($"{username} done");
+                }
+                catch (Exception e)
+                {
+                    NotFound.Add(username);
+                    Console.WriteLine($"Error getting {username} image.  Exception: {e.Message}.  {e.StackTrace}");
+                }
             }
-            catch(Exception e)
+            else
             {
-                notFound.Add(username);
-                Console.WriteLine($"Error getting {username} image.  Exception: {e.Message}.  {e.StackTrace}");
+                AlreadyExistsCount++;
             }
+        }
+
+        private static bool ImageExists(string username)
+        {
+            return File.Exists($"{_imagesFolder}/{username}.jpg");
         }
     }
 }
