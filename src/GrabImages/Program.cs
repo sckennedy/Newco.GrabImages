ccbadddc7b558948f1e2ed4a5b8601f8775aec6c
Simon Kennedy <simon@skennedy.co.uk>
2018-08-28 08:30:59 +0100

    Added routine to remove images not in the list of usernames

--- src/GrabImages/Program.cs
+++ src/GrabImages/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Net;
 
 namespace GrabImages
@@ -11,8 +12,10 @@ namespace GrabImages
         private static string _imagesFolder = @"C:\Code\Images";
         private static string _usernamesFile = @"C:\code\usernames.txt";
         private static readonly List<string> NotFound = new List<string>();
-        public static int AlreadyExistsCount = 0;
-        
+        public static int AlreadyExistsCount;
+        public static int NumberOfImagesRemoved;
+        private static string[] _usernames;
+
         static void Main(string [] args)
         {
             if (!string.IsNullOrEmpty(args[0]))
@@ -36,9 +39,9 @@ namespace GrabImages
                 return;
             }
 
-            string[] usernames = File.ReadAllLines(_usernamesFile);
+            _usernames = File.ReadAllLines(_usernamesFile);
 
-            foreach (string username in usernames)
+            foreach (string username in _usernames)
             {
                 DownloadImage(username);
             }
@@ -50,9 +53,12 @@ namespace GrabImages
                 Console.WriteLine($"Images not found  file created {fileName}");
             }
 
-            Console.WriteLine($"{usernames.Length} Usernames.");
+            RemoveUnnecessaryImages();
+
+            Console.WriteLine($"{_usernames.Length} Usernames.");
             Console.WriteLine($"{AlreadyExistsCount} images already downloaded.");
-            Console.WriteLine($"{usernames.Length - AlreadyExistsCount} new downloads.");
+            Console.WriteLine($"{_usernames.Length - AlreadyExistsCount} new downloads.");
+            Console.WriteLine($"{NumberOfImagesRemoved} removed.");
 
             Console.WriteLine("Press any key to exit.");
             Console.ReadKey();
@@ -94,6 +100,29 @@ namespace GrabImages
         private static bool ImageExists(string username)
         {
             return File.Exists($"{_imagesFolder}/{username}.jpg");
+        }
+
+        private static void RemoveUnnecessaryImages()
+        {
+            var d = new DirectoryInfo(_imagesFolder);
+            var files = d.GetFiles("*.jpg");
+            var filesToRemove = new List<FileInfo>();
+
+            foreach (var file in files)
+            {
+                var filename = file.Name.Remove(file.Name.Length - 4);
+                if (!_usernames.Contains(filename))
+                {
+                    filesToRemove.Add(file);
+                }
+            }
+
+            NumberOfImagesRemoved = filesToRemove.Count;
+
+            foreach (var fileInfo in filesToRemove)
+            {
+                fileInfo.Delete();
+            }
         }
     }
 }
